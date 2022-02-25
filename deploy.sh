99dbf40ae6d4f666176d50132a704c4c79653e51
Kaiz <realkaiz7@gmail.com>
2022-02-25 15:53:22 +0800

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker build -t kaiz/cloud-run-test .
 gcloud builds submit --tag gcr.io/pathforge-v1/kaiz/cloud-run-test .
-gcloud run deploy cloud-run-test --image gcr.io/pathforge-v1/kaiz/cloud-run-test --port=80 --allow-unauthenticated --region=asia-southeast1
+gcloud run deploy cloud-run-test --image gcr.io/pathforge-v1/kaiz/cloud-run-test --port=80 --allow-unauthenticated --region=asia-southeast1 --platform=managed
